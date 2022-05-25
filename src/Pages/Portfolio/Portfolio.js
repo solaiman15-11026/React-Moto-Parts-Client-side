6a711bb0b9dbc7fdaa8e44f3bfe50bb8cf9279a6
solaiman15-11026 <samimunna49@gmail.com>
2022-05-25 22:36:19 +0600

    data

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -8,16 +8,16 @@ import './Portfolio.css'
 
 const Portfolio = () => {
     return (
-        <Container>
-            <div className=' mt-4'>
-                <div className='port'>
-                    <div>
-                        <img src={profile} alt="" srcset="" className='' />
+        <div className='cover-body '>
+            <Container className=''>
+                <div className='port '>
+                    <div className='mt-4 '>
+                        <img className='rounded' src={profile} alt="" srcset="" className='' />
                     </div>
                     <div className='' >
                         <div className='rty'>
-                            <h1 class="text-4xl font-bold text-red-500 mb-2"> Md.Solaiman Sheik</h1>
-                            <p class="py-6 reviwe">My name is Solaiman, and I'm a professional web developer. I enjoy coding and consider it a hobby. I'm here to assist you and to develop your website.
+                            <h1 class="text-4xl font-bold text-pink-500 mb-2"> Md.Solaiman Sheik</h1>
+                            <p class="py-6 review">My name is Solaiman, and I'm a professional web developer. I enjoy coding and consider it a hobby. I'm here to assist you and to develop your website.
                                 I can assist you in best describing yourself. Regardless of how complicated your web page is, I'll find a simple solution and accomplish your work. Keep in touch with me.</p>
                             <button class="btn btn-primary mt-2">More About Me <FontAwesomeIcon className='text-white text-2xl pl-2' icon={faArrowRight}></FontAwesomeIcon></button>
                         </div>
@@ -79,10 +79,10 @@ const Portfolio = () => {
                 </div> <br />   <br /> <br /> <br />
 
                 <div className='w-100 mx-auto mb-24 '>
-                    <h1 className='text-center  mb-3 text-4xl uppercase'> <span className='text-blue-800'>EDUCATION Qualification</span></h1>
+                    <h1 className='text-center  mb-3 text-4xl uppercase'> <span className='text-pink-700'>EDUCATION Qualification</span></h1>
                     <hr style={{ width: '43%', margin: '0  auto' }} className='mb-4 ' />
                     <div >
-                        <div className='bg-black text-white p-5 mr-5 mb-5 rounded-3 shadow-1xl'>
+                        <div className='bg-blue-900 text-white p-5 mr-5 mb-5 rounded-3 shadow-1xl'>
                             <h2 className='text-2xl pl-10'><FontAwesomeIcon className='text-warning text-2xl pl-2 mr-2' icon={faGraduationCap}></FontAwesomeIcon><span className='font-bold'>Bechelor Degree-</span> <span>Daffodil International University</span></h2>
                             <p className='pl-10 py-3 '>I have completed my Bechelor Degree from Daffodil International University
                                 in Computer Science & Engineering. I got CGPA 3.25 out of 4.00
@@ -91,7 +91,7 @@ const Portfolio = () => {
                         </div>
                     </div>
 
-                </div>
+                </div> <br /> <br />
 
 
 
@@ -101,10 +101,10 @@ const Portfolio = () => {
 
 
 
+            </Container >
+        </div >
 
-            </div>
 
-        </Container>
     );
 };
 
